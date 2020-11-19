2732688eed9c149df7c7bd0dac97e67a0287c10a
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-11-19 15:50:22 -0700

    init commit

---
+++ scheduler.py
@@ -0,0 +1,38 @@
+import time
+import schedule
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+import requests
+import os
+from dotenv import load_dotenv
+import pymongo
+load_dotenv()
+
+KEY = os.getenv('KEY')
+MONGO_URL = os.getenv('MONGO_URL')
+client = pymongo.MongoClient(MONGO_URL)
+
+
+def job():
+    data = {}
+    url = ('http://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           f'apiKey={KEY}')
+    response = requests.get(url)
+    json = response.json()
+
+    titles = []
+    for i in json['articles']:
+        pol = TextBlob(i['title']).sentiment.polarity
+        titles.append({"title": i['title'], "pol": pol})
+
+    five_worst = sorted((i for i in titles), key=lambda k: k['pol'])[0:6]
+    data['title'] = five_worst[0]['title']
+
+    db = client["badnews"]
+    collection = db["articles"]
+    response = collection.insert_one(data)
+
+
+if __name__ == "__main__":
+    job()
