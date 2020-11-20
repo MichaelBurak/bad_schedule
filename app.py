74daf131d588acdf9764517d00aad1caf9924169
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-11-20 04:40:49 -0700

    added vue client

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+import os
+from dotenv import load_dotenv
+import pymongo
+load_dotenv()
+
+KEY = os.getenv('KEY')
+MONGO_URL = os.getenv('MONGO_URL')
+client = pymongo.MongoClient(MONGO_URL)
+db = client["badnews"]
+collection = db["articles"]
+
+# initialize Flask application
+app = Flask(__name__)
+
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route("/", methods=["GET"])
+def entry():
+    data = collection.find_one(
+        sort=[('_id', pymongo.DESCENDING)]
+    )['title']
+    return jsonify(str(data))
+
+
+if __name__ == '__main__':
+    app.run()
--- scheduler.py
+++ scheduler.py
@@ -6,14 +6,18 @@ import requests
 import os
 from dotenv import load_dotenv
 import pymongo
+import datetime
 load_dotenv()
 
 KEY = os.getenv('KEY')
 MONGO_URL = os.getenv('MONGO_URL')
 client = pymongo.MongoClient(MONGO_URL)
 
+db = client["badnews"]
+collection = db["articles"]
 
-def job():
+
+def analyze_news():
     data = {}
     url = ('http://newsapi.org/v2/top-headlines?'
            'country=us&'
@@ -28,9 +32,6 @@ def job():
 
     five_worst = sorted((i for i in titles), key=lambda k: k['pol'])[0:6]
     data['title'] = five_worst[0]['title']
-
-    db = client["badnews"]
-    collection = db["articles"]
     response = collection.insert_one(data)
 
 
